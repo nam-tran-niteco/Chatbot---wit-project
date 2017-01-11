bb13d703dfc54ae5bc8164541ba2a05f5aa28690
Tran Hoai Nam <namth27494@gmail.com>
2017-01-12 00:40:01 +0700

    [H] Style bot response

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,23 +6,40 @@ $(document).ready( function () {
 
         var chat = $('#chat').val();
 
-        $.ajax({
-            url: '/chat',
-            type: 'POST',
-            dataType: 'json',
-            data: JSON.stringify({
-                chat: chat
-            }),
-            contentType: "application/json; charset=utf-8",
-            complete: function (xhr, status) {
-                console.log(status);
-            },
-            success: function (data) {
-                console.log(data);
-            },
-            error: function (err) {
-                console.log(err);
-            }
-        })
+        // check user's input being empty or not
+        if( chat ) {
+
+            // append user's input to chabox
+            $('.chat-content').append('<p class="user-chat">' + chat + '</p>');
+            $('#chat').val('');
+
+            $.ajax({
+                url: '/chat',
+                type: 'POST',
+                dataType: 'json',
+                data: JSON.stringify({
+                    chat: chat
+                }),
+                contentType: "application/json; charset=utf-8",
+                complete: function (xhr, status) {
+                    console.log(status);
+                },
+                success: function (data) {
+                    console.log(data);
+                    $('.waiting').text('Xin chao');
+                    $('#chat').prop('disabled', false);
+                    $('.chat-content').scrollTop($('.chat-content').height());
+                    $('#chat').focus();
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            });
+            
+            $('.chat-content').append('<p class="bot-chat waiting">' + 'bot is typing...' + '</p>');
+            $('#chat').prop('disabled', true);
+            $('.chat-content').scrollTop($('.chat-content').height());
+        }
+
     });
 } );
